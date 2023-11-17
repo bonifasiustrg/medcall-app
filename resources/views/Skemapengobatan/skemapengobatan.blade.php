6cf43bccafed282d63a29168f917af652f1d68bf
Risna Zahira <risnazahir@student.telkomuniversity.ac.id>
2023-11-17 17:20:41 +0700

    Create skemapengobatan.blade.php

---
+++ resources/views/Skemapengobatan/skemapengobatan.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MedCall</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h2>Form Pendaftaran Pengobatan</h2>
+        <form>
+            <div class="form-group">
+                <label for="nama">Nama*</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="Bagus Safutra">
+            </div>
+            <div class="form-group">
+                <label for="poli">Pilih Poli*</label>
+                <select class="form-control" id="poli" name="poli">
+                    <option>Poli Penyakit dalam</option>
+                    <!-- Tambahkan opsi lainnya di sini -->
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="tanggal">Tanggal Konsultasi*</label>
+                <input type="date" class="form-control" id="tanggal" name="tanggal">
+            </div>
+            <div class="form-group">
+                <label for="dokter">Pilih Dokter*</label>
+                <select class="form-control" id="dokter" name="dokter">
+                    <option>dr. Indra Perkasa</option>
+                    <!-- Tambahkan opsi lainnya di sini -->
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">SIMPAN</button>
+        </form>
+    </div>
+</body>
+</html>
