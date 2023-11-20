530e42bb524f341278deb29746c30d21fd29cc1d
Aqilah Fedura Ilahi <aqilahfedura@student.telkomuniversity.ac.id>
2023-11-20 13:32:51 +0700

    Update pengingatjanji.blade.php

--- resources/views/pengingatjanji.blade.php
+++ resources/views/pengingatjanji.blade.php
@@ -1,99 +1,94 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>MEDCALL</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pengingat Janji Konsultasi</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
 
-  <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">MEDCALL</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tentang</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Kontak</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pencarian</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Akun</a>
-            </li>
-          </ul>
+<div class="container">
+
+  <h1>Pengingat Janji Konsultasi</h1>
+
+  <div class="row">
+
+    <div class="col-md-6">
+
+      <div class="card">
+        <div class="card-header">
+          Kalender Konsultasi
+        </div>
+        <div class="card-body">
+
+          <div id="calendar"></div>
+
         </div>
       </div>
-    </nav>
 
-    <div class="row">
-      <div class="col-md-12">
-        <h1>Jadwal Konsultasi Mendatang</h1>
-      </div>
     </div>
-    <div class="row">
-      <div class="col-md-2">
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th>Sen</th>
-              <th>Sel</th>
-              <th>Rab</th>
-              <th>Kam</th>
-              <th>Jum</th>
-              <th>Sab</th>
-              <th>Min</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              </tr>
-            <tr>
-              </tr>
-            <tr>
-              </tr>
-            <tr>
-              </tr>
-            <tr>
-              </tr>
-          </tbody>
-        </table>
+
+    <div class="col-md-6">
+
+      <div class="card">
+        <div class="card-header">
+          Notes Pengingat
+        </div>
+        <div class="card-body">
+
+          <p>
+            Tanggal 24 Desember 2024, Anda memiliki janji konsultasi dengan dr. Suktomo.
+          </p>
+
+        </div>
       </div>
-      <div class="col-md-10">
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th>Waktu</th>
-              <th>Dokter</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>14:20</td>
-              <td>Dr. Indra Perkasa</td>
-            </tr>
-            <tr>
-              <td>14:40</td>
-              <td>Dr. Dewi Lestari</td>
-            </t </tr>
-          </tbody>
-        </table>
-      </div>
+
     </div>
+
   </div>
 
-  <footer class="footer bg-dark text-light">
-    <div class="container">
-      <span class="text-muted">Copyright &copy; 2023 MEDCALL</span>
-    </div>
-  </footer>
+</div>
 
-  </body>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+
+<script>
+
+$(document).ready(function() {
+
+  // Initialize the calendar
+  var calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
+    locale: 'id',
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    defaultView: 'month',
+    events: [
+      {
+        title: 'Konsultasi dengan dr. Suktomo',
+        start: moment('2024-12-24', 'YYYY-MM-DD'),
+        end: moment('2024-12-24', 'YYYY-MM-DD') + 'T10:00:00'
+      }
+    ]
+  });
+
+  // Add notes pengingat
+  calendar.on('eventClick', function(event) {
+    if (event.title === 'Konsultasi dengan dr. Suktomo') {
+      alert('Tanggal ' + event.start.format('DD MMMM YYYY') + ', Anda memiliki janji konsultasi dengan dr. Suktomo.');
+    }
+  });
+
+  // Render the calendar
+  calendar.render();
+
+});
+
+</script>
+
+</body>
 </html>
