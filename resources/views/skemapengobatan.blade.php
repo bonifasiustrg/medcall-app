5e9e2bf0f4382e5b34e5a5b223789513e93cedc1
Risna Zahira <risnazahir@student.telkomuniversity.ac.id>
2023-11-20 13:41:37 +0700

    Update skemapengobatan.blade.php

--- resources/views/skemapengobatan.blade.php
+++ resources/views/skemapengobatan.blade.php
@@ -10,13 +10,14 @@
     }
     .navbar-atas {
       background-color: #EFF5FF;
+      color: #000000;
     }
   </style>
 </head>
 <body>
 
-    <!-- Navbar atas -->
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-atas">
+  <!-- Navbar atas -->
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-atas">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">MEDCALL</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,17 +41,23 @@
       </div>
     </div>
   </nav>
+
+  <!-- Formulir -->
   <div class="container">
     <h2>Form Pendaftaran Pengobatan</h2>
     <form>
         <div class="form-group">
             <label for="nama">Nama*</label>
-            <input type="text" class="form-control" id="nama" name="nama" placeholder="Bagus Safutra">
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama Lengkap Anda">
         </div>
         <div class="form-group">
             <label for="poli">Pilih Poli*</label>
-            <select class="form-control" id="poli" name="poli">
-                <option>Poli Penyakit dalam</option>
+            <select class="form-control" id="poli" name="poli" onchange="updateDokterOptions()">
+                <option>Poli Umum</option>
+                <option>Poli Gigi dan Mulut</option>
+                <option>Poli Penyakit Dalam</option>
+                <option>Poli Jiwa</option>
+                <option>Poli Mata</option>
                 <!-- Tambahkan opsi lainnya di sini -->
             </select>
         </div>
@@ -61,9 +68,8 @@
         <div class="form-group">
             <label for="dokter">Pilih Dokter*</label>
             <select class="form-control" id="dokter" name="dokter">
-                <option>dr. Indra Perkasa</option>
-                <!-- Tambahkan opsi lainnya di sini -->
-               </select>
+                <!-- Opsi dokter akan diperbarui dengan JavaScript -->
+            </select>
         </div>
         <button type="submit" class="btn btn-primary">SIMPAN</button>
     </form>
@@ -75,6 +81,36 @@
       <a class="navbar-brand" href="#">
     </div>
   </nav>
+
+  <script>
+    function updateDokterOptions() {
+      var poliSelect = document.getElementById('poli');
+      var dokterSelect = document.getElementById('dokter');
+      
+      // Kosongkan opsi dokter terlebih dahulu
+      dokterSelect.innerHTML = "";
+
+      // Tambahkan opsi dokter berdasarkan pilihan poli
+      if (poliSelect.value === "Poli Penyakit Dalam") {
+        var dokters = ["dr. A", "dr. B", "dr. C"];
+      } else if (poliSelect.value === "Poli Gigi dan Mulut") {
+        var dokters = ["dr. D", "dr. E", "dr. F"];
+      } else {
+        // Tambahkan logika untuk poli lain jika diperlukan
+        var dokters = [];
+      }
+
+      for (var i = 0; i < dokters.length; i++) {
+        var option = document.createElement('option');
+        option.value = dokters[i];
+        option.text = dokters[i];
+        dokterSelect.appendChild(option);
+      }
+    }
+
+    // Panggil fungsi saat halaman dimuat
+    window.onload = updateDokterOptions;
+  </script>
 
 </body>
 </html>
