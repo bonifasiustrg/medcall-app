274e52ae7710e921ac94bca43e608093b140b5c7
Aqilah Fedura Ilahi <aqilahfedura@student.telkomuniversity.ac.id>
2023-11-20 15:05:37 +0700

    Update pengingatjanji.blade.php

--- resources/views/pengingatjanji.blade.php
+++ resources/views/pengingatjanji.blade.php
@@ -3,37 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MedCall</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Styled Bootstrap Calendar</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css"/>
+    <style>
+        body {
+            font-family: 'Lato', sans-serif; /* Gunakan font Lato */
+            background-color: #EFF5FF; /* Warna latar belakang */
+        }
+
+        .container {
+            margin-top: 50px;
+        }
+
+        #calendar {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #f8f9fa;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-left: 0; /* Geser ke kiri */
+        }
+    </style>
+    <!-- Menambahkan font Lato dari Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato&display=swap">
 </head>
 <body>
     <div class="container">
-        <h2 class="text-center mt-5">MedCall</h2>
-        <h4 class="text-center mb-5">Jadwal Konsultasi Mendatang</h4>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Hari</th>
-                    <th scope="col">Jam</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Senin</td>
-                    <td>14.20</td>
-                </tr>
-                <tr>
-                    <td>Selasa</td>
-                    <td>14.40</td>
-                </tr>
-                <!-- Add more rows for other days -->
-            </tbody>
-        </table>
-        <h4 class="text-center mt-5">Contacts</h4>
-        <p class="text-center">Konsultasi dengan dr. Indra Perkasa</p>
+        <div id="calendar"></div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#calendar').fullCalendar({
+                // Tambahkan opsi dan peristiwa Anda di sini
+            });
+        });
+    </script>
 </body>
 </html>
