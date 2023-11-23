9223f3ff796dae995430c36a80e854ce98aeda75
MuhamadKhoirFahniNurIslami <119027207+MuhamadKhoirFahniNurIslami@users.noreply.github.com>
2023-11-23 22:54:45 +0700

    change emergency

--- resources/views/darurat.blade.php
+++ resources/views/darurat.blade.php
@@ -1,35 +1,73 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
   <meta charset="UTF-8">
-  <title>Emergency Call</title>
+  <title>MEDCALL</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <style>
+    body {
+      background-color: #EFF5FF;
+    }
+    .navbar-atas {
+      background-color: #EFF5FF;
+      color: #000000;
+    }
+  </style>
 </head>
 <body>
-  <div id="emergency-call-screen">
-    <div id="emergency-call-header">
-      <h1>Emergency Call</h1>
+
+  <!-- Navbar atas -->
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-atas" style="background-color: #527BC0;">
+    <div class="container-fluid">
+    <a><img src="{{ asset('image/navbar.png') }}" alt="" class="img-fluid w-52"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tentang</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kontak</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pencarian</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Akun</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div id="emergency-call-body">
-      <p>Silahkan menekan tombol WhatsApp atau tombol telpon untuk melakukan panggilan emergency.</p>
-      <div id="emergency-call-buttons">
-        <button id="emergency-call-whatsapp">WhatsApp</button>
-        <button id="emergency-call-phone">Telpon</button>
+  </nav>
+<div class="text-center">
+    <img src="{{ asset('image/emergency.png') }}" alt="Gambar contoh" class="rounded">
+</div>
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+    <i class="fas fa-phone"></i>
+</button>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="myModalLabel">Nomor Telepon</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>085694532122</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
       </div>
     </div>
   </div>
+</div>
 
-  <script>
-    const emergencyCallNumber = "085694532122";
 
-    document.getElementById("emergency-call-whatsapp").addEventListener("click", () => {
-      // Buka aplikasi WhatsApp dengan nomor darurat
-      window.open("https://wa.me/" + emergencyCallNumber);
-    });
+  
 
-    document.getElementById("emergency-call-phone").addEventListener("click", () => {
-      // Lakukan panggilan telepon ke nomor darurat
-      window.open("tel:" + emergencyCallNumber);
-    });
-  </script>
-</body>
-</html>
