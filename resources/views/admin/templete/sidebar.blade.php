644f98421bb88e89b2653f3610ef9b143fbeac03
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2024-01-05 08:19:25 +0700

    Improve Queue Systen UI

--- resources/views/admin/templete/sidebar.blade.php
+++ resources/views/admin/templete/sidebar.blade.php
@@ -73,19 +73,15 @@
                 </p>
                 </a>
             </li>
-
-            <li class="nav-item">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
+            <li class="nav-menu">
+                <a href="{{ route('logout') }}" class="nav-link inactive" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
                     <i class="nav-icon fas fa-th"></i>
-                    
-                    {{ __('Logout') }}
+                    <p>
+                        Logout
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
                 </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
             </li>
             </ul>
         </nav>
